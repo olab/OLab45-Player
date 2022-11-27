a831d493e39d853a14c10c9a908967a3d404b94f
Corey <reg@pathrider.ca>
2022-11-26 19:58:09 -0700

    Added login handlers

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import {
-  Button, FormControl, Input,
-  InputLabel, Paper, Typography,
+  Button, FormControl, Input, Backdrop, CircularProgress,
+  InputLabel, Paper, Typography, Snackbar
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import log from 'loglevel';
 import styles from './styles';
 import { config } from '../../config';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 async function loginUserAsync(credentials) {
 
@@ -36,25 +41,47 @@ const Login = ({ authActions, classes }) => {
 
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
+  const [open, setOpen] = React.useState(false);
+  const [inProgress, setInProgress] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleSubmit = async e => {
+
     e.preventDefault();
+
+    setInProgress(true);
+
     const response = await loginUserAsync({
       username,
       password
-    });    
+    });
 
+    if (response.statusCode != 200) {
+      setOpen(true);
+    }
+
+    setInProgress(false);
     authActions.setToken(response, false);
     authActions.setUserName(username);
+
   }
 
   return (
     <main className={classes.main}>
-      <Paper className={classes.paper}>
+      {<Paper className={classes.paper}>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form onSubmit={handleSubmit}>
+        { inProgress && <p><CircularProgress color="inherit" /></p>}        
+        { !inProgress && <form onSubmit={handleSubmit}>
           <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="username">Username</InputLabel>
             <Input
@@ -81,8 +108,13 @@ const Login = ({ authActions, classes }) => {
           >
             Sign in
           </Button>
-        </form>
-      </Paper>
+        </form>}
+        <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            Login error!
+          </Alert>
+        </Snackbar>
+      </Paper>}
     </main>
   )
 }
