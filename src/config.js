617f7848ca02449f8053d08f06430e9df839ac44
Corey Wirun <reg@pathrider.ca>
2022-12-14 15:28:28 +0000

    Changed Player config settings

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Constants.js
 const prod = {
-    API_URL: 'https://olabprdapi.azurewebsites.net/olab/api/v3',
-    TTALK_HUB_URL: 'https://dev.olab.ca/olab/turktalk'
+    API_URL: 'https://logan.cardinalcreek.ca/olab/api/v3',
+    TTALK_HUB_URL: 'https://logan.cardinalcreek.ca/olab/turktalk'
 };
 
 const dev = {
     API_URL: 'https://localhost:5001/olab/api/v3',
-    TTALK_HUB_URL: 'https://localhost:5001/turktalk'
+    TTALK_HUB_URL: 'https://localhost:5001/olab/turktalk'
 };
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
