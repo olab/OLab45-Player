aff28e13b8b3375b1beb891ed8f245bb95ef6e80
Corey <reg@pathrider.ca>
2023-01-26 07:10:46 -0700

    Added stack to logger

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,11 +1,26 @@
 import log from 'loglevel';
 
+function getErrorObject(){
+  try { throw Error('') } catch(err) { return err; }
+}
+
 const LogEnable = () => {
   log.enableAll();
 }
 
 const LogError = (message) => {
-  log.error(`ERR: ${message}`);
+
+  try {
+    var err = getErrorObject();
+    var caller_line = err.stack.split("\n")[4];
+    var index = caller_line.indexOf("at ");
+    var location = caller_line.slice(index+2, caller_line.length);      
+    log.error(`ERR: ${message} at ${location}`);
+
+  } catch (error) {
+    log.error(`ERR: ${message} at ?????`);    
+  }
+
 }
 
 const LogInfo = (message) => {
