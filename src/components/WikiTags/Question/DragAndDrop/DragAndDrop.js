b1dc24f3387b2dfc81c4462f4d8d8fb13e9a6c25
Corey <reg@pathrider.ca>
2023-02-14 14:11:08 -0700

    Added react-beautiful-dnd

--- src/components/WikiTags/Question/DragAndDrop/DragAndDrop.js
+++ src/components/WikiTags/Question/DragAndDrop/DragAndDrop.js
@@ -1,13 +1,11 @@
 // @flow
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import { withStyles } from '@material-ui/core/styles';
 import { Log, LogInfo, LogError } from '../../../../utils/Logger';
 import log from 'loglevel';
-import DragDropContainer from './DragDropContainer';
 
 import styles from '../../styles.module.css';
 import siteStyles from '../../site.module.css';
@@ -22,14 +20,23 @@ class OlabDragAndDropQuestion extends React.Component {
       ...props.props
     };
 
+    this.grid = 8;
+
     // Binding this keyword  
-    this.setInProgress = this.setInProgress.bind(this)
+    this.setInProgress = this.setInProgress.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
 
   }
 
+  setInProgress(inProgress) {
+
+    this.setState({ showProgressSpinner: inProgress });
+    log.debug(`set progress spinner: ${inProgress}`);
+  }
+  
   setValue = (responses) => {
 
-    const question = this.state.question;    
+    const question = this.state.question;
     log.debug(`responses`);
 
     let values = [];
@@ -37,9 +44,9 @@ class OlabDragAndDropQuestion extends React.Component {
     for (const iterator of responses) {
       log.debug(` ${iterator.response}(${iterator.id})`);
       values.push(iterator.id);
-    }    
+    }
 
-    if ( typeof question.previousValue == 'undefined' ) {
+    if (typeof question.previousValue == 'undefined') {
       question.previousValue = null;
     }
     else {
@@ -77,46 +84,88 @@ class OlabDragAndDropQuestion extends React.Component {
       onSubmitResponse(responseState);
     }
   }
-  
-  setInProgress(inProgress) {
 
-    this.setState({ showProgressSpinner: inProgress });
-    log.debug(`set progress spinner: ${inProgress}`);
+  // a little function to help us with reordering the result
+  reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    border: '1px solid gray',
+    padding: '0.5rem 1rem',
+    marginBottom: '.5rem',
+    backgroundColor: 'white',
+    cursor: 'move',
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+  });
+
+  getListStyle = isDraggingOver => ({
+    background: "lightgrey",
+    padding: this.grid,
+    width: this.state.question.width ? this.state.question.width : 450
+  });
+
+  onDragEnd(result) {
+
+    var { question } = this.state;
+
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const responses = this.reorder(
+      this.state.question.responses,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setValue(responses);
   }
 
-  setIsDisabled(disabled) {
-
-    this.setState({ disabled: disabled });
-    log.debug(`set disabled: ${disabled}`);
-  }
-
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   render() {
-
-    const {
-      id,
-      name,
-      question,
-      // disabled
-    } = this.state;
-
-    log.debug(`OlabDragAndDropQuestion render '${name}'`);
-
     try {
-
-      let width = 300;
-      if (question.width) {
-        width = question.width;
-      }
-
       return (
-        <div className={`${styles['qudraganddrop']} ${siteStyles[id]}`} id={`${id}`}>
-          <DndProvider backend={HTML5Backend}>
-            <DragDropContainer 
-              width={width} 
-              onChange={this.setValue}
-              responses={question.responses} />
-          </DndProvider>
-        </div>
+
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={this.getListStyle()}
+              >
+                {this.state.question.responses.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.name} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={this.getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.response}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       );
 
     } catch (error) {
@@ -126,7 +175,6 @@ class OlabDragAndDropQuestion extends React.Component {
         </>
       );
     }
-
   }
 
 }
