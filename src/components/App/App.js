b409eec4808f171ed5de94915365b6771fb78435
Corey <reg@pathrider.ca>
2023-04-24 08:56:41 -0600

    revert App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useParams,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import Home from "../Home/Home";
 import Header from "../Header/Header";
@@ -9,13 +15,11 @@ import log from "loglevel";
 import Player from "../Player/Player";
 import useToken from "./useToken";
 import { config } from "../../constants";
-import {
-  loginExternalUserAsync,
-  loginAnonymousUserAsync,
-} from "../../services/api";
+import { loginExternalUserAsync } from "../../services/api";
 
 function App() {
   const { authActions } = useToken();
+  const [token, setToken] = useState(0);
   const reactVersion = process.env.REACT_APP_VERSION;
 
   log.debug(JSON.stringify(process.env));
@@ -24,41 +28,8 @@ function App() {
   // comes with it's own access token)
   const searchParams = new URLSearchParams(document.location.search);
   const queryToken = searchParams.get("token");
-
-  function processUrl() {
-    let mapId = null;
-    let nodeId = null;
-
-    const urlParts = window.location.pathname.split("/");
-    if (urlParts.length == 4) {
-      mapId = urlParts[2];
-      if (isNaN(mapId)) {
-        mapId = null;
-      } else {
-        mapId = Number(mapId);
-      }
-
-      nodeId = urlParts[3];
-      if (isNaN(nodeId)) {
-        nodeId = null;
-      } else {
-        nodeId = Number(nodeId);
-      }
-
-      return [mapId, nodeId];
-    }
-
-    return [null, null];
-  }
-
-  const [mapId, nodeId] = processUrl();
-
-  const [state, setState] = useState({
-    token: null,
-    isExternal: queryToken != null,
-    mapId: mapId,
-    nodeId: nodeId,
-  });
+  const isExternal = queryToken != null;
+  const [externalLoginStatus, setExternalLoginStatus] = useState(null);
 
   useEffect(() => {
     if (!token) {
@@ -94,7 +65,7 @@ function App() {
 
       log.debug(`useEffect: token: ${authActions.getToken()}`);
     }
-  }, []);
+  }, [token, authActions]);
 
   const isExpired = authActions.isExpiredSession();
   log.debug(`render: token: ${token}`);
@@ -146,6 +117,69 @@ function App() {
     </div>
   );
 }
+
+// function processCookieForTokenAsync(cookieString) {
+
+//   if (!cookieString) {
+//     log.debug(`No external cookies set`);
+//     return null;
+//   }
+
+//   var token = extractExternalToken(cookieString);
+//   if (!token) {
+//     return null;
+//   }
+
+//   let url = `${config.API_URL}/auth/loginexternal`;
+
+//   log.debug(`loginExternal(${token}) url: ${url})`);
+
+//   var body = {
+//     "ExternalToken": token
+//   };
+
+//   return fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   })
+//     .then(
+//       data => data.json()
+//     )
+
+// }
+
+// function extractExternalToken(cookieStr) {
+
+//   try {
+
+//     log.debug(`parsing: '${cookieStr}')`);
+
+//     const parseCookie = str =>
+//       str
+//         .split(';')
+//         .map(v => v.split('='))
+//         .reduce((acc, v) => {
+//           acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+//           return acc;
+//         }, {});
+
+//     let cookies = parseCookie(cookieStr);
+//     log.debug(`Cookie: ${JSON.stringify(cookies, null, 2)}`);
+
+//     if ('external_token' in cookies) {
+//       log.debug(`External token: ${cookies.external_token}`);
+//       return cookies.external_token;
+//     }
+
+//     return null;
+
+//   } catch (error) {
+//     LogError(error);
+//   }
+// }
 
 function NoMatch() {
   let location = useLocation();
