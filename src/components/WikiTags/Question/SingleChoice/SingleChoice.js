1af3dae64238a22c9786a10207e9d22f578522b7
elhardoum <elhardoum@outlook.com>
2023-07-13 21:47:09 -0600

    use 1 and -1 to check for correct/incorrect question response feedback/indicators

--- src/components/WikiTags/Question/SingleChoice/SingleChoice.js
+++ src/components/WikiTags/Question/SingleChoice/SingleChoice.js
@@ -147,7 +147,7 @@ class OlabSinglePickQuestion extends React.Component {
       // is_correct and feedback.
       if (selectedIndex == response.id) {
         // test for 'correct' answer
-        if (response.isCorrect > 0 && question.showAnswerIndicators) {
+        if (response.isCorrect == 1 && question.showAnswerIndicators) {
           correctnessIndicator = (
             <>
               <CheckIcon style={{ color: "green" }} />
@@ -157,7 +157,7 @@ class OlabSinglePickQuestion extends React.Component {
         }
 
         // test for 'incorrect' answer
-        if (response.isCorrect == 0 && question.showAnswerIndicators) {
+        if (response.isCorrect == -1 && question.showAnswerIndicators) {
           correctnessIndicator = (
             <>
               <CloseIcon style={{ color: "red" }} />
