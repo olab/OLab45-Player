9314e82a2d450a68b84456762ac7a6b455082232
Corey Wirun <reg@pathrider.ca>
2024-09-04 14:58:24 -0600

    "referrer

--- src/services/api.js
+++ src/services/api.js
@@ -8,6 +8,34 @@ if (config?.API_RETRY_COUNT) {
   retryCount = Number(config.API_RETRY_COUNT);
 }
 
+var policies = [
+  // Referer will never be set.
+  "no-referrer",
+
+  // Referer will not be set when navigating from HTTPS to HTTP.
+  "no-referrer-when-downgrade",
+
+  // Full Referer for same-origin requests, and no Referer for cross-origin requests.
+  "same-origin",
+
+  // Referer will be set to just the origin, omitting the URL's path and search.
+  "origin",
+
+  // Referer will be set to just the origin except when navigating from HTTPS to HTTP,
+  // in which case no Referer is sent.
+  "strict-origin",
+
+  // Full Referer for same-origin requests, and bare origin for cross-origin requests.
+  "origin-when-cross-origin",
+
+  // Full Referer for same-origin requests, and bare origin for cross-origin requests
+  // except when navigating from HTTPS to HTTP, in which case no Referer is sent.
+  "strict-origin-when-cross-origin",
+
+  // Full Referer for all requests, whether same- or cross-origin.
+  "unsafe-url",
+];
+
 async function internetJsonFetch(
   method,
   url,
@@ -36,6 +64,7 @@ async function internetJsonFetch(
   while (tries++ < retryCount) {
     try {
       settings.signal = AbortSignal.timeout(30000);
+      settings = { ...settings, referrerPolicy: "no-referrer-when-downgrade" };
       const response = await fetch(url, settings);
 
       let data = {};
