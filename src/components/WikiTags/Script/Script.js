b19ed640625704db64ea28e13bf24413db566f35
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-02-11 08:20:31 -0700

    added script in blob

--- src/components/WikiTags/Script/Script.js
+++ src/components/WikiTags/Script/Script.js
@@ -18,8 +18,15 @@ class OlabScriptTag extends OlabTag {
     this.loadSnippet();
   }
 
+  updateObject(newObject) {
+    log.debug(`updating object`);
+    this.props.props.onUpdateScopedObjects(newObject);
+  }
+
   loadSnippet = async () => {
     try {
+      const { debug, script } = this.state;
+
       const response = await fetch(
         "https://olabfiles.blob.core.windows.net/files/Maps/1451/scriptTest.html"
       );
@@ -39,11 +46,43 @@ class OlabScriptTag extends OlabTag {
   };
 
   render() {
-    return (
-      <div id="snippetContainer">
-        {/* The HTML snippet will be loaded here */}
-      </div>
-    );
+    const { debug, script } = this.state;
+    const { name } = this.props;
+
+    log.debug(`OlabScriptTag render '${name}'`);
+
+    try {
+      if (script == null) {
+        throw new Error(`'${this.props.name}' not found`);
+      }
+
+      if (debug.disableWikiRendering) {
+        return (
+          <>
+            <br />
+            <b>[[SCRIPT:{name}]]</b>
+            <br />
+            <textarea rows="10" cols="50" defaultValue={script.source} />
+            <br />
+            <br />
+          </>
+        );
+      }
+
+      return (
+        <div id="snippetContainer">
+          {/* The HTML snippet will be loaded here */}
+        </div>
+      );
+    } catch (error) {
+      return (
+        <>
+          <b>
+            [[SCRIPT:{name}]] error: {error.message}
+          </b>
+        </>
+      );
+    }
   }
 }
 
